0e21fe81918c5a451792b13c4b0d76637c95df1d
Paweł ZIemann <p-ziemann@wp.pl>
2024-03-21 14:06:09 +0100

    Dodanie funkcji do wyswietlania logow i env variables

--- PythonFlaskProject/flask_project.py
+++ PythonFlaskProject/flask_project.py
@@ -1,17 +1,40 @@
 from flask import Flask
 from random import randint
+import logging
+import os
 
+logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
 
+# Main page
 @app.route("/", methods=["GET"])
 def main_page():
-    return "Main page</p>"
+    # Log messages of different levels
+    log_messages = log_levels()
+    env_info = get_envs()
+    return f"<h1>Log levels:</h1>{log_messages}<br><h1>Environment variables:</h1>{env_info}"
 
-#endpoint get-item creation
+# Endpoint get-item creation
 @app.route("/get-item", methods=["GET"])
 def random_number():
-    random_num = randint(1,1000) #Random number 1-1000
+    random_num = randint(1, 1000)  # Random number 1-1000    
     return f"{random_num}"
+
+
+# Function to retrieve log levels and messages
+def log_levels():
+    log_messages = []
+    for level in ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']:
+        message = f"{level} log info"
+        logging.getLogger(__name__).log(getattr(logging, level), message)
+        log_messages.append(message)
+    return "<br>".join(log_messages)
+def get_envs():
+    env_info = []
+    for key, value in os.environ.items():
+        env_info.append(f"{key}: {value}")
+    return "<br>".join(env_info)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
